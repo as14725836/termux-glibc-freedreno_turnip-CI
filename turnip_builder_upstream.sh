50879d8161b7325821f77ee3a8502718e1721a5b
Asai <152795869+as14725836@users.noreply.github.com>
2026-07-04 03:04:38 +0800

    Update turnip_builder_upstream.sh

--- turnip_builder_upstream.sh
+++ turnip_builder_upstream.sh
@@ -98,7 +98,7 @@ build_lib_for_linux(){
             -Dgallium-drivers=zink,freedreno,virgl,softpipe \
             -Dgallium-rusticl-enable-drivers=freedreno \
             -Dshader-cache-default=true \
-            -Dvulkan-drivers=freedreno,swrast,virtio \
+            -Dvulkan-drivers=freedreno \
             -Dfreedreno-kmds=kgsl \
             -Dvulkan-layers=anti-lag \
             -Dvideo-codecs=all \
